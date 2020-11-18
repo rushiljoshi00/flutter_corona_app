ec02e5a3b40991100da8ca4251914b9b97f91094
rushiljoshi00 <rushil.joshi@ryerson.ca>
2020-11-18 13:13:13 -0500

    app complete after tutorial

---
+++ lib/app/services/api.dart
@@ -0,0 +1,42 @@
+import 'package:coronavirus_respt_api_course/app/services/api_keys.dart';
+import 'package:flutter/foundation.dart';
+
+// enumeration is used for defining named constant values
+// Endpoint is the enumeration type name
+// cases, etc are the comma seperated list of identifiers 
+enum Endpoint {
+  cases,
+  casesSuspected,
+  casesConfirmed,
+  deaths,
+  recovered,
+}
+
+class API {
+  API({@required this.apiKey});
+  final String apiKey;
+
+  factory API.sandbox() => API(apiKey: APIKeys.ncovSandboxKey);
+
+  static final String host = 'ncov2019-admin.firebaseapp.com';
+
+  Uri tokenUri() => Uri(
+        scheme: 'https',
+        host: host,
+        path: 'token',
+      );
+
+  Uri endpointUri(Endpoint endpoint) => Uri(
+        scheme: 'https',
+        host: host,
+        path: _paths[endpoint],
+      );
+
+  static Map<Endpoint, String> _paths = {
+    Endpoint.cases: 'cases',
+    Endpoint.casesSuspected: 'casesSuspected',
+    Endpoint.casesConfirmed: 'casesConfirmed',
+    Endpoint.deaths: 'deaths',
+    Endpoint.recovered: 'recovered',
+  };
+}
